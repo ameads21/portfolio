b8392dd638587dc0750acec13733d7de911dcf7a
Aaron Meads <aaron.meads@gmail.com>
2021-06-26 20:20:34 -0600

    Adding Django to portfolio

--- routes/home.py
+++ routes/home.py
@@ -13,7 +13,7 @@ languages = [KeyValue("HTML", "90%"), KeyValue(
 
 frameworks = [KeyValue("Bootstrap", "100%"), KeyValue("VSCode", "95%"), KeyValue("Jinja", "90%"), KeyValue("Flask", "85%"), KeyValue(
     "Flask-WTForms", "85%"), KeyValue("Flask-SQLAlchemy", "85%"), KeyValue("Axios", "80%"), KeyValue("PostgreSQL", "75%"),
-    KeyValue("Heroku", "70%"), KeyValue("Big-O Notation", "70%"), KeyValue("jQuery", "65%"), KeyValue("Express", "55%")]
+    KeyValue("Heroku", "70%"), KeyValue("Big-O Notation", "70%"), KeyValue("jQuery", "65%"), KeyValue("Express", "55%"), KeyValue("Django", "25%")]
 
 projects = [Project("Meads' Meal-kit Planner", "Python, SQL, HTML, CSS, JavaScript", "../static/mealPlannerLogo.jpg", "https://meads-meal-planner-app.herokuapp.com/"), Project("JeoTool/Game Show Creator", "Node, React, SQL, HTML, CSS", "../static/jeotool.png", "https://jeotool.herokuapp.com/"),
             Project("Guess The Capital", "HTML, CSS, JavaScript", "../static/guessthatcapitalLogo.jpg", "https://ameads21.github.io/guess-the-capital/"), Project("Ward Website", "Python, SQL, HTML, CSS, JavaScript", "../static/zoom.jpg", "https://ch12ward.herokuapp.com/"), Project(
