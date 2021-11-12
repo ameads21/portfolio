bab21c7268f760e10969ed45b1973c10cad7a20b
Aaron <aaron.meads@gmail.com>
2021-11-11 21:17:31 -0700

    Updating framework details

--- routes/home.py
+++ routes/home.py
@@ -11,9 +11,9 @@ languages = [KeyValue("HTML", "90%"), KeyValue(
     "Python", "80%"), KeyValue("GML", "75%"), KeyValue("JavaScript", "75%"),
     KeyValue("SQL", "65%"), KeyValue("ReactJS", "65%"), KeyValue("NodeJS", "55%"), KeyValue("CSS", "50%"), KeyValue("Git", "35%")]
 
-frameworks = [KeyValue("Bootstrap", "100%"), KeyValue("VSCode", "95%"), KeyValue("Jinja", "90%"), KeyValue("Flask", "85%"), KeyValue(
-    "Flask-WTForms", "85%"), KeyValue("Flask-SQLAlchemy", "85%"), KeyValue("Axios", "80%"), KeyValue("PostgreSQL", "75%"),
-    KeyValue("Heroku", "70%"), KeyValue("Big-O Notation", "70%"), KeyValue("jQuery", "65%"), KeyValue("Express", "55%"), KeyValue("Django", "25%")]
+frameworks = [KeyValue("Bootstrap", "100%"), KeyValue("VSCode", "95%"), KeyValue("Jinja", "90%"), KeyValue("Flask", "80%"), KeyValue(
+    "Flask-WTForms", "85%"), KeyValue("Flask-SQLAlchemy", "85%"), KeyValue("Axios", "80%"), KeyValue("PostgreSQL", "75%"), KeyValue("Pandas Python", "75%"),
+    KeyValue("Heroku", "70%"), KeyValue("Django", "70%"), KeyValue("Big-O Notation", "70%"), KeyValue("jQuery", "65%"), KeyValue("Express", "35%")]
 
 projects = [Project("Meads' Meal-kit Planner", "Python, SQL, HTML, CSS, JavaScript", "../static/mealPlannerLogo.jpg", "https://meads-meal-planner-app.herokuapp.com/"), Project("JeoTool/Game Show Creator", "Node, React, SQL, HTML, CSS", "../static/jeotool.png", "https://jeotool.herokuapp.com/"),
             Project("Guess The Capital", "HTML, CSS, JavaScript", "../static/guessthatcapitalLogo.jpg", "https://ameads21.github.io/guess-the-capital/"), Project("Ward Website", "Python, SQL, HTML, CSS, JavaScript", "../static/zoom.jpg", "https://ch12ward.herokuapp.com/"), Project(
